0751aee89c5295aa6f161a3c5f2b29e50deec3ff
aqwenaaa <aqueenahapsari22@smk.belajar.id>
2025-03-24 16:02:48 +0700

    Praktikum 2: Penerapan Layouting-modifikasi

--- Minggu5_Baru/routes/web.php
+++ Minggu5_Baru/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,6 +19,12 @@ Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+/* Jobsheet 5 */   
+Route::get('/', [WelcomeController::class, 'index']);
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
