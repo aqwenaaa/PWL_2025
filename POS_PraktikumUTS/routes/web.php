c06a175f3a31c51e3d771fc644d4f0ad161051bf
aqwenaaa <aqueenahapsari22@smk.belajar.id>
2025-04-09 16:34:08 +0700

    Implementasi Praktikum 2

--- POS_PraktikumUTS/routes/web.php
+++ POS_PraktikumUTS/routes/web.php
@@ -1,17 +1,81 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ItemController;
+
+Route::get('/hello', function () { return 'Hello World';
+});
+
+Route::get('/world', function () { return 'World';
+});
+
+Route::get('/user/{name}', function ($name) { return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+Route::get('/user/{name?}', function ($name=null) {
+return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') { return 'Nama saya '.$name;
+});
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
+use App\Http\Controllers\UserProfileController;
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+Route::get('/user/profile', 
+    [UserProfileController::class, 'show'])->name('profile');
+
+//Generating URL
+//$url = route('profile');
+//Generating Redirects
+// return redirect()->route('profile');
+
+Route::get('/user/{name?}', function ($name='John') { return 'Nama saya '.$name;
+});
+
+Route::view('/welcome', 'welcome', ['name' => 'Aqueena']);
+
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+use App\Http\Controllers\PageController;
+// Route untuk halaman utama
+Route::get('/', [PageController::class, 'index']);
+
+// Route untuk halaman about
+Route::get('/about', [PageController::class, 'about']);
+
+// Route untuk halaman artikel dengan ID dinamis
+Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+use App\Http\Controllers\PhotoController;
+// Membuat semua route CRUD untuk PhotoController
+Route::resource('photos', PhotoController::class);
+
+Route::get('/greeting', function () {
+    return view('hello', ['name' => 'Aqueena']);
+});
+
+Route::get('/greeting', function () {
+    return view('blog.hello', ['name' => 'Aqueena']);
+});
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
