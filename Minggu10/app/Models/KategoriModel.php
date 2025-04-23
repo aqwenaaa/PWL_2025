ef19d7a0890b8fc17caad6279e5403c277c238b9
aqwenaaa <aqueenahapsari22@smk.belajar.id>
2025-04-24 02:11:44 +0700

    TUGAS m_barang, m_kategori, m_user implementasi CRUD API

--- Minggu10/app/Models/KategoriModel.php
+++ Minggu10/app/Models/KategoriModel.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class KategoriModel extends Model
 {
@@ -10,6 +12,10 @@ class KategoriModel extends Model
 
     protected $table = 'm_kategori';
     protected $primaryKey = 'kategori_id';
-    protected $fillable = ['kategori_kode', 'kategori_nama'];
-
+    protected $guarded = [ ];
+    protected $fillable = [
+        'kategori_id',
+        'kategori_kode',
+        'kategori_nama',
+    ];
 }
