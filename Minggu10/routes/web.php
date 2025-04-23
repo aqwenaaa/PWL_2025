5212b904f528daf757dcb4563e8bc831b67be62c
aqwenaaa <aqueenahapsari22@smk.belajar.id>
2025-04-23 17:37:15 +0700

    Praktikum 2 : POST dan GET

--- Minggu10/routes/web.php
+++ Minggu10/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::pattern('id', '[0-9]+'); // Pastikan parameter {id} hanya berupa angka
